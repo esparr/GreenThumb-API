7b4f67b896809d151d7cc948bef5d08e50a53779
Em Sparr <amberem@gmail.com>
2021-09-15 13:17:22 -0400

    Registers models in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
+from core.models import User, Question, Answer
 from django.contrib import admin
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    pass
