d321e6e4b61d324f6f8328738c39927b233b1834
Em Sparr <amberem@gmail.com>
2021-09-15 15:30:31 -0400

    Fixed dunder for user.

--- core/models.py
+++ core/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    def __repr__(self):
+        return f"<User username={self.username}>"
+    def __str__(self):
+        return self.username
     pass
 
 
