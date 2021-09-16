500483cc88ff4d24759d89a53e680136445052dc
Em Sparr <amberem@gmail.com>
2021-09-16 10:27:42 -0400

    Added UserAdmin to admin.py to fix password hashing issues.

--- core/admin.py
+++ core/admin.py
@@ -1,15 +1,17 @@
 from core.models import User, Question, Answer
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 
 # Register your models here.
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    pass
+
+
+admin.site.register(User, UserAdmin)
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ['title', 'owner', 'created_at']
     pass
+
 
 @admin.register(Answer)
 class AnswerAdmin(admin.ModelAdmin):
